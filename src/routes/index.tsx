3ffaaef59176d3681441b6737cec823139b9ce97
cashblaze127 <cashblaze127@gmail.com>
2022-09-23 22:55:00 -0700

    remove casino route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,16 @@ import { useRoutes } from 'react-router-dom';
 
 import MainRoutes from './MainRoutes';
 import MyBetsRoutes from './MyBetsRoutes';
-import CasinoRoutes from './CasinoRoutes';
+// import CasinoRoutes from './CasinoRoutes';
 import ProfileRoutes from './ProfileRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
 import Error from 'views/pages/Error';
 
 export default function ThemeRoutes() {
-    return useRoutes([MainRoutes, CasinoRoutes, ProfileRoutes, MyBetsRoutes, AuthenticationRoutes, { path: '*', element: <Error /> }]);
+    return useRoutes([
+        MainRoutes, 
+        // CasinoRoutes, 
+        ProfileRoutes, 
+        MyBetsRoutes, 
+        AuthenticationRoutes, { path: '*', element: <Error /> }]);
 }
